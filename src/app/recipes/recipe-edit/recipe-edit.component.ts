9eca7cdb194ada7a0f31368f8f7163b5e36d2a19
awtw <alec.wang.tpe@gmail.com>
2020-01-02 23:32:13 +0800

    form edit recipes syncing Html with form

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,26 +1,46 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
+import { FormGroup, FormControl } from '@angular/forms';
+import { RecipeService } from '../recipe.service';
 
 @Component({
-  selector: 'app-recipe-edit',
-  templateUrl: './recipe-edit.component.html',
-  styleUrls: ['./recipe-edit.component.css']
+	selector: 'app-recipe-edit',
+	templateUrl: './recipe-edit.component.html',
+	styleUrls: [ './recipe-edit.component.css' ]
 })
 export class RecipeEditComponent implements OnInit {
-  id: number;
-  edithMode = false;
+	id: number;
+	edithMode = false;
+	recipeForm: FormGroup;
 
-  constructor(private route: ActivatedRoute) { }
+	constructor(private route: ActivatedRoute, private recipeService: RecipeService) {}
 
-  ngOnInit() {
-    this.route.params
-      .subscribe(
-        (params: Params) => {
-          this.id =+params['id'],
-            this.edithMode = params['id'] != null; // I think this is like Ternary Operator (三元運算子)
-          // console.log(this.edithMode);
-        }
-      )
-  }
+	ngOnInit() {
+		this.route.params.subscribe((params: Params) => {
+			this.id = +params['id'];
+			this.edithMode = params['id'] != null;
+			this.initForm();
+		});
+	}
+	private initForm() {
+		let recipeName = '';
+		let recipeImagePath = '';
+		let recipeDescription = '';
 
+		if (this.edithMode) {
+			const recipe = this.recipeService.getRecipe(this.id);
+			recipeName = recipe.name;
+			recipeImagePath = recipe.imagePath;
+			recipeDescription = recipe.description;
+		}
+
+		this.recipeForm = new FormGroup({
+			name: new FormControl(recipeName),
+			imagePath: new FormControl(recipeImagePath),
+			description: new FormControl(recipeDescription)
+		});
+	}
+	onSubmit() {
+		console.log(this.recipeForm);
+	}
 }
