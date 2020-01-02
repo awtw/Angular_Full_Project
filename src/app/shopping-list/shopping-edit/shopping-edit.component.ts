7b9ec04af0d4e918ede38da3bfd1a5f7ae836f5e
alec wang <alex0123321@gmail.com>
2020-01-02 15:47:05 +0800

    update exist item

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component, OnInit, OnDestroy, ViewChild } from '@angular/core';
 import { Ingredient } from 'src/app/shared/ingredient.model';
 import { ShoppingListService } from '../shopping-list.service';
 import { NgForm } from '@angular/forms';
@@ -9,15 +9,21 @@ import { Subscription } from 'rxjs';
   styleUrls: ['./shopping-edit.component.css']
 })
 export class ShoppingEditComponent implements OnInit, OnDestroy {
+  @ViewChild('f', {static: true}) slForm: NgForm;
   subscription: Subscription;
   editMode = false;
   editItemIndex: number;
+  editedItem: Ingredient;
   constructor(private shoppListService: ShoppingListService) { }
 
   onAddItem(form: NgForm){
     const value = form.value;
     const newIngredient = new Ingredient(value.name,value.amount);
+    if(this.editMode){
+      this.shoppListService.updateIngredient(this.editItemIndex, newIngredient);
+    } else {
     this.shoppListService.addIngredient(newIngredient);
+    }
     // this.ingredientAdded.emit(newIngredient);
   }
 
@@ -27,6 +33,11 @@ export class ShoppingEditComponent implements OnInit, OnDestroy {
         (index: number) => {
           this.editItemIndex = index;
           this.editMode = true;
+          this.editedItem = this.shoppListService.getIngredient(index);
+          this.slForm.setValue({
+            name: this.editedItem.name,
+            amount: this.editedItem.amount
+          })
         }
       );
   }
--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -18,6 +18,11 @@ export class ShoppingListService {
   getIngredients(){
     return this.ingredients.slice();
   }
+
+  getIngredient(index: number){
+    return this.ingredients[index];
+  }
+
   addIngredient(ingredient: Ingredient){
     this.ingredients.push(ingredient);
     this.ingredientChanged.next(this.ingredients.slice());
@@ -25,6 +30,11 @@ export class ShoppingListService {
 
   addIngredients(ingredients: Ingredient[]){
     this.ingredients.push(...ingredients);
+    this.ingredientChanged.next(this.ingredients.slice());
+  }
+
+  updateIngredient(index: number, newIngredient: Ingredient){
+    this.ingredients[index] = newIngredient;
     this.ingredientChanged.next(this.ingredients.slice());
   }
 }
