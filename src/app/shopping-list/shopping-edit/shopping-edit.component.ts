bec25a290613890acd008626a9bc1ebdf86c024e
alec wang <alex0123321@gmail.com>
2020-01-02 14:39:01 +0800

    add validation to the form [pattern to check if number greater than 0]

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,22 +1,19 @@
-import { Component, OnInit, ElementRef, ViewChild, EventEmitter, Output} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Ingredient } from 'src/app/shared/ingredient.model';
 import { ShoppingListService } from '../shopping-list.service';
+import { NgForm } from '@angular/forms';
 @Component({
   selector: 'app-shopping-edit',
   templateUrl: './shopping-edit.component.html',
   styleUrls: ['./shopping-edit.component.css']
 })
 export class ShoppingEditComponent implements OnInit {
-  @ViewChild('nameInput',{static: true}) nameInputRef: ElementRef;
-  @ViewChild('amountInput', { static: true }) amountInputRef: ElementRef;
-  // @Output() ingredientAdded = new EventEmitter<Ingredient>();
 
   constructor(private shoppListService: ShoppingListService) { }
 
-  onAddItem(){
-    const ingName = this.nameInputRef.nativeElement.value;
-    const ingAmount = this.nameInputRef.nativeElement.value;
-    const newIngredient = new Ingredient(ingName,ingAmount);
+  onAddItem(form: NgForm){
+    const value = form.value;
+    const newIngredient = new Ingredient(value.name,value.amount);
     this.shoppListService.addIngredient(newIngredient);
     // this.ingredientAdded.emit(newIngredient);
   }
