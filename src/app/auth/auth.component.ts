1a3551711986a63ddf0293510988ee98452631cc
awtw <alec.wang.tpe@gmail.com>
2020-01-06 22:34:53 +0800

    add error message

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -3,45 +3,46 @@ import { NgForm } from '@angular/forms';
 import { AuthService } from './auth.service';
 
 @Component({
-  selector: 'app-auth',
-  templateUrl: './auth.component.html',
-  styleUrls: ['./auth.component.css']
+	selector: 'app-auth',
+	templateUrl: './auth.component.html',
+	styleUrls: [ './auth.component.css' ]
 })
 export class AuthComponent implements OnInit {
+	constructor(private authService: AuthService) {}
 
-  constructor(private authService: AuthService) { }
+	ngOnInit() {}
 
-  ngOnInit() {
-  }
+	isLoginMode = true;
+	isLoading = false;
+	error: string = null;
 
-  isLoginMode = true;
-  isLoading = false;
+	onSwitchMode() {
+		this.isLoginMode = !this.isLoginMode;
+	}
+	onSubmit(form: NgForm) {
+		console.log(form.value);
+		if (!form.valid) {
+			return;
+		}
+		const email = form.value.email;
+		const password = form.value.password;
+		this.isLoading = true;
+		if (this.isLoginMode) {
+			//...
+		} else {
+			this.authService.signup(email, password).subscribe(
+				(resData) => {
+					console.log(resData);
+					this.isLoading = false;
+				},
+				(error) => {
+					console.log(error);
+					this.error = 'An error occurred';
+					this.isLoading = false;
+				}
+			);
+		}
 
-  onSwitchMode(){
-    this.isLoginMode = !this.isLoginMode;
-  }
-  onSubmit(form: NgForm){
-    console.log(form.value);
-    if(!form.valid){
-      return;
-    }
-    const email = form.value.email;
-    const password = form.value.password;
-    this.isLoading = true;
-    if(this.isLoginMode){
-      //...
-    } else {
-      this.authService.signup(email, password).subscribe(resData => {
-        console.log(resData);
-        this.isLoading = false;
-      }, error => {
-        console.log(error);
-        this.isLoading = false;
-      });
-    }
-
-
-    form.reset();
-  }
-
+		form.reset();
+	}
 }
